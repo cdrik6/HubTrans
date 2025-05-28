37fb36841933eac1fb44aa701a0b852676e7496d
Cedric Aguillon <caguillo@student.42.fr>
2025-05-29 01:39:04 +0200

    node update

--- main.js
+++ main.js
@@ -6,23 +6,23 @@
 /*   By: caguillo <caguillo@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/05/28 00:42:02 by caguillo          #+#    #+#             */
-/*   Updated: 2025/05/28 01:11:54 by caguillo         ###   ########.fr       */
+/*   Updated: 2025/05/29 01:10:35 by caguillo         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-const fastify = require('fastify')();
+const fastify = require('fastify')({logger: true});
 
-fastify.get('/hello', (request, reply) => {
-    reply.send({
-        message: 'Hello Fastify'
-    });
-});
+// Declare a route
+fastify.get('/hello',  function (request, reply) {
+  reply.send({message: 'hello world'})
+})
 
-fastify.listen({ port: 3000 }, (err, address) => {
-    if(err) {
-        console.error(err);
-        process.exit(1);
-    }
-    console.log(`Server listening at: ${address}`);
-});
+// Run the server!
+fastify.listen({ port: 3000 }, function (err, address) {
+  if (err) {
+    fastify.log.error(err)
+    process.exit(1)
+  }
+  // Server is now listening on ${address}
+})
 
