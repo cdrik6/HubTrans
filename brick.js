46b27150da5a2e74fea1acfd5aea9bf77be3a266
Cedric Aguillon <caguillo@student.42.fr>
2025-06-05 01:27:03 +0200

    pong

--- brick.js
+++ brick.js
@@ -6,34 +6,47 @@
 /*   By: caguillo <caguillo@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/06/02 21:36:04 by caguillo          #+#    #+#             */
-/*   Updated: 2025/06/03 01:04:52 by caguillo         ###   ########.fr       */
+/*   Updated: 2025/06/05 01:22:25 by caguillo         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 let canvas = document.getElementById("myCanvas");
+let para = document.getElementById("myP");
 let ctx = canvas.getContext("2d");
 let ballRadius = 10;
 let d = -2;
 let x = canvas.width / 2;
-let y = canvas.height - 30;
-let dx = 2;
-let dy = -2;
-let signX = 1;
-let signY = 1;
-let paddleHeight = 80, paddleWidth = 15;
+let y = canvas.height / 2;
+const ballSpeed = 3;
+let dx = ballSpeed; //* getRandomSign();
+let dy = ballSpeed; //* getRandomSign();
+let signX = getRandomSign();
+let signY = getRandomSign();
+let paddleHeight = 80, paddleWidth = 3* ballRadius / 2;
 let paddle1Y = (canvas.height - paddleHeight) / 2;
 let paddle2Y = (canvas.height - paddleHeight) / 2;
-var upPressed1 = false;
-var downPressed1 = false;
-var upPressed2 = false;
-var downPressed2 = false;
+let upPressed1 = false;
+let downPressed1 = false;
+let upPressed2 = false;
+let downPressed2 = false;
+let loose1 = false;
+let loose2 = false;
+// let padTouch1 = false;
+// let padTouch2 = false;
+const padSpeed = 3;
 document.addEventListener("keydown", keyDownHandler, false);
 document.addEventListener("keyup", keyUpHandler, false);
 
 function drawBall() {	
 	ctx.beginPath();		
 	ctx.arc(x, y, ballRadius, 0, Math.PI * 2);
-	ctx.fillStyle = "#0095DD";
+	ctx.fillStyle = "rgba(255, 0, 0, 1)";
+	ctx.fill();
+	ctx.arc(x-2*signX, y-2*signY, ballRadius, 0, Math.PI * 2);
+	ctx.fillStyle = "rgba(255, 0, 0, 0.6)";
+	ctx.fill();
+	ctx.arc(x-4*signX, y-4*signY, ballRadius, 0, Math.PI * 2);
+	ctx.fillStyle = "rgba(255, 0, 0, 0.3)";
 	ctx.fill();
 	ctx.closePath();	
 }
@@ -48,49 +61,89 @@ function drawPaddles() {
 }
 
 function draw() {
-	ctx.clearRect(0, 0, canvas.width, canvas.height);	  
-	// ctx.beginPath();
-	// ctx.arc(240, 160, 50, 0, Math.PI * 2, false);
-	// ctx.fillStyle = "#FF0000";
-	// ctx.fill();
-	// // ctx.lineWidth = 1;
-	// // ctx.arc(260, 200, 10, 0, Math.PI * 2, false);
-	// // ctx.fillStyle = "green";
-	// // ctx.fill();
-	// d = -d
-	// ctx.lineWidth = 20 - d;
-	// ctx.strokeStyle = "rgba(0, 0, 0, 1)";	
-	// ctx.stroke();	
-	// ctx.closePath();	
+	ctx.clearRect(0, 0, canvas.width, canvas.height);	  		
 	drawBall();
-	if (x + ballRadius >= canvas.width || x - ballRadius <= 0)
+	drawPaddles(); 
+	// check
+	if (x + ballRadius >= canvas.width)
+		loose2 = true; // signX = -signX;
+	else if (x - ballRadius <= 0)
+		loose1 = true; // signX = -signX;
+	else if (y + ballRadius == canvas.height || y - ballRadius == 0)
+	{
+		signY = -signY;
+		// signX = getRandomSign();	--> option
+	}
+	// else if (x + ballRadius*0 + paddleWidth >= canvas.width && y + ballRadius*0 >= paddle2Y && y + ballRadius*0 <= paddle2Y + paddleHeight)		
+	// {
+	// 	signX = -signX;
+	// 	// signY = getRandomSign();
+	// 	// padTouch1 = true;
+	// }
+	// else if (x - ballRadius*0 - paddleWidth <= 0 && y + ballRadius*0 >= paddle1Y && y + ballRadius*0 <= paddle1Y + paddleHeight)
+	// {
+	// 	signX = -signX;
+	// 	// signY = getRandomSign();
+	// 	// padTouch2 = true;
+	// }
+	else if (x + ballRadius + paddleWidth > canvas.width && distBallPad2() < 0)
+	{
 		signX = -signX;
-	if (y + ballRadius >= canvas.height || y - ballRadius <= 0)
-		signY = -signY;	
-	x = x + signX*dx;
-	y = y + signY*dy;	
-	//
+	}
+	else if (x - ballRadius - paddleWidth <	 0 && distBallPad1() < 0)
+	{
+		signX = -signX;
+	}
+		
+	// next
+	if (!loose1 && !loose2)
+	{
+		x = x + signX*dx;
+		y = y + signY*dy;		
+	}
+	else
+	{
+		x = canvas.width / 2; //+ signX*dx;
+		y = canvas.height / 2; //+ signY*dy;	
+		loose1 = false;
+		loose2 = false;
+		dx = ballSpeed; // * getRandomSign(); // console.log(dx);
+		dy = ballSpeed; // * getRandomSign(); // console.log(dy);
+		signX = getRandomSign();
+		signY = getRandomSign();
+	}
+	padMovement();	
+	requestAnimationFrame(draw);
+}
+
+// function checkPad1()
+// {
+// 	if (x + ballRadius > )
+// }
+
+function padMovement()
+{
 	if (upPressed2 == true)
 	{
 		if (paddle2Y > 0 )
-			paddle2Y = paddle2Y - 1;		
+			paddle2Y = paddle2Y - padSpeed;		
 	}
 	if (downPressed2 == true)
 	{
 		if (paddle2Y + paddleHeight < canvas.height )
-			paddle2Y = paddle2Y + 1;
+			paddle2Y = paddle2Y + padSpeed;
 	}
 	if (upPressed1 == true)
 	{
 		if (paddle1Y > 0 )
-			paddle1Y = paddle1Y - 1;		
+			paddle1Y = paddle1Y - padSpeed;		
 	}
 	if (downPressed1 == true)
 	{
 		if (paddle1Y + paddleHeight < canvas.height )
-			paddle1Y = paddle1Y + 1;
-	}	
-	drawPaddles(); 	
+			paddle1Y = paddle1Y + padSpeed;
+	}
+	// para.innerHTML = distBallPad2();
 }
 
 function keyDownHandler(e) {
@@ -103,7 +156,7 @@ function keyDownHandler(e) {
   else if (e.key == "w") {
     upPressed1 = true;	
   }
-  else if (e.key == "z") {
+  else if (e.key == "x") {
     downPressed1 = true;	
   }
 }
@@ -118,10 +171,54 @@ function keyUpHandler(e) {
   else if (e.key == "w") {
     upPressed1 = false;	
   }
-  else if (e.key == "z") {
+  else if (e.key == "x") {
     downPressed1 = false;	
   }
 }
 
+// setInterval(draw, 20);
+draw();
 
-setInterval(draw, 10);
+function getRandomIntInclusive(min, max)
+{
+  const minCeiled = Math.ceil(min);
+  const maxFloored = Math.floor(max);
+  return Math.floor(Math.random() * (maxFloored - minCeiled + 1) + minCeiled); // The maximum is inclusive and the minimum is inclusive
+}
+
+function getRandomSign()
+{
+	let s = getRandomIntInclusive(0, 1);
+	if (s ==0)
+		return (1);
+	else 
+		return (-1);
+}
+
+function distBallPad2()
+{
+	const xP = canvas.width - paddleWidth;
+	const yP = paddle2Y + paddleHeight;	
+
+	if (y >= paddle2Y && y <= yP)
+		return (xP - (x + ballRadius));
+	if (y > yP)
+		return (Math.sqrt((x - xP) * (x - xP) + (y - yP) * (y - yP)) - ballRadius);
+	if (y < paddle2Y)
+		return (Math.sqrt((x - xP) * (x - xP) + (y - paddle2Y) * (y - paddle2Y)) - ballRadius);		
+}
+
+function distBallPad1()
+{
+	const xP = paddleWidth;
+	const yP = paddle1Y + paddleHeight;
+	
+
+	if (y >= paddle1Y && y <= yP)
+		return (xP - (x + ballRadius));
+	if (y > yP)
+		return (Math.sqrt((x - xP) * (x - xP) + (y - yP) * (y - yP)) - ballRadius);
+	if (y < paddle1Y)
+		return (Math.sqrt((x - xP) * (x - xP) + (y - paddle1Y) * (y - paddle1Y)) - ballRadius);
+		
+}
