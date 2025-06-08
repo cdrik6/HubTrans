a3fea3904a9253202992f040b205f432758c4e69
Cedric Aguillon <caguillo@student.42.fr>
2025-06-09 01:35:41 +0200

    pong server side

--- fastify-ws/client.js
+++ fastify-ws/client.js
@@ -1,47 +1,55 @@
 const output = document.getElementById('output');
 
-document.getElementById('canvas').onclick = () => {
-	const clt_skt = new WebSocket('ws://localhost:3000/pong');
+// document.getElementById('canvas').onclick = () => {
+	const clt_wskt = new WebSocket('ws://localhost:3000/pong');
 
-	// clt_skt.addEventListener('open', () => {
+	// clt_wskt.addEventListener('open', () => {
 	// 	output.textContent += 'Connected to WebSocket\n';
-	// 	clt_skt.send('Hello from client');
+	// 	clt_wskt.send('Hello from client');
 	// });
 
-	// clt_skt.addEventListener('message', event => {
+	// clt_wskt.addEventListener('message', event => {
 	// 	output.textContent += 'Received ici: ' + event.data + '\n';
 	// });
 
-	clt_skt.addEventListener('close', () => {
+	clt_wskt.addEventListener('close', () => {
 		output.textContent += 'WebSocket closed\n';
 	});
 
-	clt_skt.addEventListener('error', err => {
+	clt_wskt.addEventListener('error', err => {
 	output.textContent += 'Error: ' + err + '\n';
 	});
 
-	clt_skt.addEventListener('message', event => {
+	clt_wskt.addEventListener('message', event => {
 		try {
 			const data = JSON.parse(event.data);
-    		console.log('Ball position:', data.ball.x, data.ball.y);
-			drawBall(data.ball.x, data.ball.y)
+    		console.log('Ball position:', data.ball.xB, data.ball.yB);
+			drawBall(data.ball.xB, data.ball.yB)
     		//console.log('Score P2:', data.score.p2);
+			draw();
+			clt_wskt.send(JSON.stringify(paddles));
 		}
 		catch (e) {
 			console.error('Invalid JSON received:', event.data);
 		}		
 	});
 
-	clt_skt.addEventListener('open', () => {
-		// output.textContent += 'Connected to WebSocket\n';
-		// clt_skt.send('Hello from client');
-		clt_skt.send(JSON.stringify(paddles));
+	clt_wskt.addEventListener('open', () => {
+		output.textContent += 'Connected to WebSocket\n';
+		// clt_wskt.send('Hello from client');
+		clt_wskt.send(JSON.stringify(paddles));
 	});
-};
+// };
 
 let canvas = document.getElementById("canvas");
 let ctx = canvas.getContext("2d");
-const ballRadius = 10;
+const ballRadius = 10; const paddleHeight = 80; const paddleWidth = 3 * ballRadius / 2;
+let paddle1Y = (canvas.height - paddleHeight) / 2; let paddle2Y = (canvas.height - paddleHeight) / 2;
+let upPressed1 = false; let downPressed1 = false; let upPressed2 = false; let downPressed2 = false;
+let paddles = {pad: {p1:paddle1Y, p2:paddle2Y} }
+const padSpeed = 3;
+document.addEventListener("keydown", keyDownHandler, false);
+document.addEventListener("keyup", keyUpHandler, false);
 
 function drawBall(x, y) {	
 	ctx.beginPath();		
@@ -51,9 +59,92 @@ function drawBall(x, y) {
 	ctx.closePath();		
 }
 
-let paddles = {
-    pad: {p1:10, p2:20}    
+function drawPaddles() {
+  ctx.beginPath();
+  ctx.rect(0, paddle1Y, paddleWidth, paddleHeight);
+  ctx.rect(canvas.width - paddleWidth, paddle2Y, paddleWidth, paddleHeight);
+  ctx.fillStyle = "#0095DD";
+  ctx.fill();
+  ctx.closePath();
 }
+
+function draw() {
+	ctx.clearRect(0, 0, canvas.width, canvas.height);	  		
+	//drawBall();
+	drawPaddles();
+	padMovement();	
+	//requestAnimationFrame(draw);
+}
+// setInterval(draw, 50);
+draw();
+
+function padMovement()
+{
+	if (upPressed2 == true)
+	{
+		if (paddle2Y > 0 )
+			paddle2Y = paddle2Y - padSpeed;
+		clt_wskt.send(JSON.stringify(paddles));
+	}
+	if (downPressed2 == true)
+	{
+		if (paddle2Y + paddleHeight < canvas.height )
+			paddle2Y = paddle2Y + padSpeed;
+		clt_wskt.send(JSON.stringify(paddles));
+	}
+	if (upPressed1 == true)
+	{
+		if (paddle1Y > 0 )
+			paddle1Y = paddle1Y - padSpeed;
+		clt_wskt.send(JSON.stringify(paddles));
+	}
+	if (downPressed1 == true)
+	{
+		if (paddle1Y + paddleHeight < canvas.height )
+			paddle1Y = paddle1Y + padSpeed;
+		clt_wskt.send(JSON.stringify(paddles));
+	}
+	paddles = {pad: {p1:paddle1Y, p2:paddle2Y} }
+	// clt_wskt.addEventListener('open', () => {
+	// 	// output.textContent += 'Connected to WebSocket\n';
+	// 	// clt_wskt.send('Hello from client');
+		
+	// });
+}
+
+function keyDownHandler(e) {
+  if (e.key == "Up" || e.key == "ArrowUp") {
+    upPressed2 = true;
+  }
+  else if (e.key == "Down" || e.key == "ArrowDown") {
+    downPressed2 = true;	
+  }
+  else if (e.key == "w") {
+    upPressed1 = true;	
+  }
+  else if (e.key == "x") {
+    downPressed1 = true;	
+  }
+}
+
+function keyUpHandler(e) {
+  if (e.key == "Up" || e.key == "ArrowUp") {
+    upPressed2 = false;
+  }
+  else if (e.key == "Down" || e.key == "ArrowDown") {
+    downPressed2 = false;
+  }
+  else if (e.key == "w") {
+    upPressed1 = false;	
+  }
+  else if (e.key == "x") {
+    downPressed1 = false;	
+  }
+}
+
+	
+
+
 
 
 // Event name
--- fastify-ws/server.js
+++ fastify-ws/server.js
@@ -20,19 +20,17 @@ fastify.ready().then(() => {
     });
     
     // bind websocketserver to the http server
-    const srv_skt = new WebSocketServer({ server , path: '/pong' });
+    const srv_wskt = new WebSocketServer({ server , path: '/pong' });
     
-    srv_skt.on('connection', (clt_skt, res) => {        
+    srv_wskt.on('connection', (clt_skt, res) => {        
         console.log('Server: Client connected');
         //
-        clt_skt.on('message', message => { 
-            /////////
-            // clt_skt.send('Hello Fastify WebSockets');
-            clt_skt.send(JSON.stringify(gameState));
-            console.log('Server received:', message.toString());
+        clt_skt.on('message', clt_msg => {
+            // clt_skt.send(JSON.stringify(gameState));
+            console.log('Server received:', clt_msg.toString());
             // paddles from client
             try {
-                const data = JSON.parse(message);
+                const data = JSON.parse(clt_msg);
                 console.log('pad1:', data.pad.p1);
                 console.log('pad2:', data.pad.p2);
             }
@@ -44,6 +42,9 @@ fastify.ready().then(() => {
         clt_skt.on('close', () => {
             console.log('Server: Client disconnected');
         });
+        //
+        setInterval(play(clt_skt), 50);
+
     });
 
     server.listen(3000, () => {
@@ -59,8 +60,114 @@ fastify.ready().then(() => {
 //     console.log(`Server listening at: ${address}`);    
 // });
 
-let gameState = {
-    ball: {x:100, y:200}
-    //score: {p1:1, p2:2}
+const canvasHeight = 320; const canvasWidth = 480; //--> to be catched ******************************//
+const ballRadius = 10; const paddleHeight = 80; const paddleWidth = 3 * ballRadius / 2;
+let paddle1Y = (canvasHeight - paddleHeight) / 2; let paddle2Y = (canvasHeight - paddleHeight) / 2;
+let x = canvasWidth / 2;
+let y = canvasHeight / 2;
+const ballSpeed = 3; let dx = ballSpeed; let dy = ballSpeed;
+let signX = getRandomSign();
+let signY = getRandomSign();
+let loose1 = false; let loose2 = false;
+let padTouch1 = false; let padTouch2 = false;
+let gameState = { ball: {xB:x, yB:y}  //score: {p1:1, p2:2}
 }
 
+function play(clt_skt) {
+    // check
+	if (x + ballRadius >= canvasWidth)
+		loose2 = true;
+	else if (x - ballRadius <= 0)
+		loose1 = true;
+	else if (y + ballRadius == canvasHeight || y - ballRadius == 0)	
+		signY = -signY;		
+	else if (distBallPad2(x, y) <= 0) 
+	{
+		if (padTouch2 === false)
+		{
+			signX = -signX;
+			padTouch2 = true;			
+		}
+		if (distBallPad2(x + signX*dx, y + signY*dy) > 0)				
+			padTouch2 = false;								
+	}		
+	else if (distBallPad1(x, y) <= 0)
+	{
+		if (padTouch1 === false)
+		{
+			signX = -signX;
+			padTouch1 = true;						
+		}			
+		if (distBallPad1(x + signX*dx, y + signY*dy) > 0)		
+			padTouch1 = false;				
+	}	
+    // next
+	if (!loose1 && !loose2)
+	{
+		x = x + signX*dx;
+		y = y + signY*dy;		
+	}
+	else
+	{
+		x = canvasWidth / 2;
+		y = canvasHeight / 2;
+		loose1 = false;
+		loose2 = false;		
+		signX = getRandomSign();
+		signY = getRandomSign();
+	}
+    //
+    gameState = { ball: {xB:x, yB:y} }
+    // clt_skt.send(JSON.stringify(gameState));
+    
+	//requestAnimationFrame(draw);
+}
+
+// setInterval(play, 50);
+
+/******
+************** tools  
+************/
+
+
+function getRandomIntInclusive(min, max)
+{
+  const minCeiled = Math.ceil(min);
+  const maxFloored = Math.floor(max);
+  return Math.floor(Math.random() * (maxFloored - minCeiled + 1) + minCeiled); // The maximum is inclusive and the minimum is inclusive
+}
+
+function getRandomSign()
+{
+	let s = getRandomIntInclusive(0, 1);
+	if (s ==0)
+		return (1);
+	else 
+		return (-1);
+}
+
+function distBallPad2(xB, yB)
+{
+	const xP = canvasWidth - paddleWidth;
+	const yP = paddle2Y + paddleHeight;	
+
+	if (yB >= paddle2Y && yB <= yP)	
+		return (xP - (xB + ballRadius));	
+	if (yB > yP)
+		return (Math.sqrt((xB - xP) * (xB - xP) + (yB - yP) * (yB - yP)) - ballRadius);
+	if (yB < paddle2Y)
+		return (Math.sqrt((xB - xP) * (xB - xP) + (yB - paddle2Y) * (yB - paddle2Y)) - ballRadius);		
+}
+
+function distBallPad1(xB, yB)
+{
+	const xP = paddleWidth;
+	const yP = paddle1Y + paddleHeight;	
+
+	if (yB >= paddle1Y && yB <= yP)		
+		return ((xB - ballRadius) - xP);	
+	if (yB > yP)
+		return (Math.sqrt((xB - xP) * (xB - xP) + (yB - yP) * (yB - yP)) - ballRadius);
+	if (yB < paddle1Y)
+		return (Math.sqrt((xB - xP) * (xB - xP) + (yB - paddle1Y) * (yB - paddle1Y)) - ballRadius);		
+}
