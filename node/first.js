db656dca0c71ce00066ec25b51b4a5b47e8b3a6d
cdrik6 <cdrik6@free.fr>
2025-07-20 06:24:29 +0200

    sql docker

--- node/first.js
+++ node/first.js
@@ -6,7 +6,7 @@
 /*   By: caguillo <caguillo@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/06/14 03:13:00 by caguillo          #+#    #+#             */
-/*   Updated: 2025/06/14 04:00:52 by caguillo         ###   ########.fr       */
+/*   Updated: 2025/07/20 03:57:50 by caguillo         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,3 +22,12 @@ const reqHandler = function (req, res) {
 }
 
 http.createServer(reqHandler).listen(8080); 
+
+// Summary of Differences
+// Feature	package.json	package-lock.json
+// Written by:	You (or tools like npm init)	Automatically by npm
+// Purpose:	Describes what your project needs	Describes exactly what was installed
+// Contains version ranges:	Yes (^, ~, etc.)	No, exact versions only
+// Tracks nested deps:	No	Yes
+// Should be committed?:	✅ Yes	✅ Yes
+// Used for install:	✅ Yes	✅ Yes — for reproducibility
