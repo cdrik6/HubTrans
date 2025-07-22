1f5dbf2e32a94518023327be863ae6a074125631
Cedric Aguillon <caguillo@student.42.fr>
2025-07-22 23:48:09 +0200

    sql.js

---
+++ fastify-ws/stats/sql.js
@@ -0,0 +1,22 @@
+// export const execute = async function (db, sql) {
+// 	return (new Promise( function (resolve, reject) {
+// 			db.exec(sql, function (err) {
+// 				if (err)
+// 					reject(err);
+// 				resolve();
+// 			});
+// 		}));
+// }
+
+// export const execute = create_db();
+
+// async function create_db(db, sql)
+// {
+// 	return (new Promise( function (resolve, reject) {
+// 			db.exec(sql, function (err) {
+// 				if (err)
+// 					reject(err);
+// 				resolve();
+// 			});
+// 		}));
+// }
